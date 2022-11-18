944e76348c39f7295fc9edd82420bbbabe720ab4
MyUsername <alberto.baglivi@gmail.com>
2022-11-18 10:07:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<!-- Start script js  -->
+<script>
+import AppHeaderTop from "./components/AppHeaderTop.vue";
+
+export default {
+
+  components : {
+    AppHeaderTop, 
+
+  }, 
+
+  data(){
+    return{
+    }
+  }
+
+}
+
+</script>
+<!-- End script js  -->
+
+
+<!-- Start Template html  -->
+<template>
+
+<!-- Start Header -->
+<header>
+  <AppHeaderTop />
+</header>
+<!-- End header -->
+
+
+<!-- Start main  -->
+<main>
+</main>
+<!-- End Main  -->
+
+<!-- Start Footer  -->
+<footer>
+</footer>
+<!-- End Footer -->
+
+</template>
+<!-- End template Html  -->
+
+
+<!-- Start style css  -->
+<style lang="scss">
+@use "./styles/general.scss" as *;
+@use "./styles/partials/variables" as *;
+
+</style>
+<!-- End style css  -->
---
+++ src/components/AppHeaderTop.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default{
+
+    name: 'AppHeaderTop', 
+
+    data(){
+        return{
+         message: "Ciao Nuovo Progetto"
+        }
+    }
+}
+
+</script>
+
+
+<template>
+{{message}}
+</template>
+
+<style lang="scss"  scoped>
+</style>
